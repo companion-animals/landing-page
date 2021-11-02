a38f5ff3a8bb1871f6c721dc8f06dcc896b3a170
Seoyul <seoyul.kim@beautydatalab.com>
2021-11-03 01:40:52 +0900

    feat: 작업시 필요한 이미지 추가

---
+++ src/images/index.ts
@@ -0,0 +1,13 @@
+const images = {
+  farad: "/images/farad.png",
+  considerEmoji: "/images/consider_emoji.png",
+  sadEmoji: "/images/sad_emoji.png",
+  tearsEmoji: "/images/tears_emoji.png",
+  embarrassmentEmoji: "/images/embarrassment_emoji.png",
+  rabbit: "/images/rabbit.png",
+  turtle: "/images/tutle.png",
+  parrotBG: "/images/parrot_bg.jpg",
+  rabbit_bg: "/images/rabbit_bg.png",
+};
+
+export default images;
