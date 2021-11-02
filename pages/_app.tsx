641ae18798b5857579a90dbc4b9af86889f6c949
lee_jjoon <seungjoon.lee@cosmochain.io>
2021-11-02 21:12:02 +0900

    gtm 컴포넌트 추가 및 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import GTM from "src/components/head/GTM";
 import LazyloadCSS from "src/components/head/LazyLoadCSS";
 
 const CustomApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <GTM />
     <LazyloadCSS href="/xeicon/xeicon.min.css" />
     <LazyloadCSS href="/styles/SpoqaHanSansNeo.css" />
     <Component {...pageProps} />
