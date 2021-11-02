0ecc753dc3632921ac421d9df447388b56061a67
Seoyul <seoyul.kim@beautydatalab.com>
2021-11-03 01:45:29 +0900

    mod: Typography 폰트 크기별로 선언해 사용하도록 변경

---
+++ src/components/atoms/text/Typographies.tsx
@@ -0,0 +1,213 @@
+import styled from "styled-components";
+
+import FMText from "src/components/atoms/text/FMText";
+
+const ExtraBold = styled(FMText).attrs({ fontType: "EXTRA_BOLD" })``;
+const Bold = styled(FMText).attrs({ fontType: "BOLD" })``;
+const Medium = styled(FMText).attrs({ fontType: "MEDIUM" })``;
+const Regular = styled(FMText).attrs({ fontType: "REGULAR" })``;
+
+export const ExtraBold56 = styled(ExtraBold)`
+  font-size: 56px;
+  line-height: 75px;
+`;
+
+export const ExtraBold28 = styled(ExtraBold)`
+  font-size: 28px;
+  line-height: 75px;
+`;
+
+export const Bold8 = styled(Bold)`
+  font-size: 8px;
+  line-height: 12px;
+`;
+
+export const Bold10 = styled(Bold)`
+  font-size: 10px;
+  line-height: 15px;
+`;
+
+export const Bold11 = styled(Bold)`
+  font-size: 11px;
+  line-height: 15px;
+`;
+
+export const Bold12 = styled(Bold)`
+  font-size: 12px;
+  line-height: 18px;
+`;
+
+export const Bold13 = styled(Bold)`
+  font-size: 13px;
+  line-height: 18px;
+`;
+
+export const Bold14 = styled(Bold)`
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const Bold15 = styled(Bold)`
+  font-size: 15px;
+  line-height: 22px;
+`;
+
+export const Bold16 = styled(Bold)`
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const Bold17 = styled(Bold)`
+  font-size: 18px;
+  line-height: 26px;
+`;
+
+export const Bold18 = styled(Bold)`
+  font-size: 18px;
+  line-height: 26px;
+`;
+
+export const Bold19 = styled(Bold)`
+  font-size: 19px;
+  line-height: 26px;
+`;
+
+export const Bold20 = styled(Bold)`
+  font-size: 20px;
+  line-height: 26px;
+`;
+
+export const Bold22 = styled(Bold)`
+  font-size: 22px;
+  line-height: 26px;
+`;
+
+export const Bold24 = styled(Bold)`
+  font-size: 24px;
+  line-height: 32px;
+`;
+
+export const Medium7 = styled(Medium)`
+  font-size: 7px;
+  line-height: 12px;
+`;
+
+export const Medium8 = styled(Medium)`
+  font-size: 8px;
+  line-height: 12px;
+`;
+
+export const Medium9 = styled(Medium)`
+  font-size: 9px;
+  line-height: 13px;
+`;
+
+export const Medium10 = styled(Medium)`
+  font-size: 10px;
+  line-height: 15px;
+`;
+
+export const Medium11 = styled(Medium)`
+  font-size: 11px;
+  line-height: 16px;
+`;
+
+export const Medium12 = styled(Medium)`
+  font-size: 12px;
+  line-height: 18px;
+`;
+
+export const Medium14 = styled(Medium)`
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const Medium15 = styled(Medium)`
+  font-size: 15px;
+  line-height: 22px;
+`;
+
+export const Medium16 = styled(Medium)`
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const Medium18 = styled(Medium)`
+  font-size: 18px;
+  line-height: 24px;
+`;
+
+export const Medium20 = styled(Medium)`
+  font-size: 20px;
+  line-height: 26px;
+`;
+
+export const Medium22 = styled(Medium)`
+  font-size: 22px;
+  line-height: 26px;
+`;
+
+export const Medium24 = styled(Medium)`
+  font-size: 24px;
+  line-height: 28px;
+`;
+
+export const Regular8 = styled(Regular)`
+  font-size: 8px;
+  line-height: 9px;
+`;
+
+export const Regular9 = styled(Regular)`
+  font-size: 9px;
+  line-height: 12px;
+`;
+
+export const Regular10 = styled(Regular)`
+  font-size: 10px;
+  line-height: 15px;
+`;
+
+export const Regular11 = styled(Regular)`
+  font-size: 11px;
+  line-height: 18px;
+`;
+
+export const Regular12 = styled(Regular)`
+  font-size: 12px;
+  line-height: 18px;
+`;
+
+export const Regular14 = styled(Regular)`
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+export const Regular15 = styled(Regular)`
+  font-size: 15px;
+  line-height: 22px;
+`;
+
+export const Regular16 = styled(Regular)`
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const Regular17 = styled(Regular)`
+  font-size: 17px;
+  line-height: 24px;
+`;
+
+export const Regular19 = styled(Regular)`
+  font-size: 19px;
+  line-height: 25px;
+`;
+
+export const Regular20 = styled(Regular)`
+  font-size: 20px;
+  line-height: 26px;
+`;
+
+export const Regular23 = styled(Regular)`
+  font-size: 23px;
+  line-height: 28px;
+`;
