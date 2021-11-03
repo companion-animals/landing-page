183cc06d8ea8ad7dec5a7ca121aa15bc7f91e548
Seoyul <seoyul.kim@beautydatalab.com>
2021-11-04 00:59:06 +0900

    mod: 토끼 이미지 이름 변경

--- src/images/index.ts
+++ src/images/index.ts
@@ -7,7 +7,7 @@ const images = {
   rabbit: "/images/rabbit.png",
   turtle: "/images/turtle.png",
   parrotBG: "/images/parrot_bg.jpg",
-  rabbit_bg: "/images/rabbit_bg.png",
+  rabbitBG: "/images/rabbit_bg.png",
   hamster: "/images/hamster.png",
   turtleFace: "images/turtle_face.png",
   mouse: "images/mouse.png",
