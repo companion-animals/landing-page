1420dbd882f655f964563d22d43329b0cf638816
Seoyul <seoyul.kim@beautydatalab.com>
2021-11-11 04:48:13 +0900

    feat: 모바일에허 솨면 작게 보이는 현상 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import LazyloadCSS from "src/components/head/LazyLoadCSS";
 
 const CustomApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1, user-scalable=no"
+      />
+    </Head>
     <LazyloadCSS href="/xeicon/xeicon.min.css" />
     <LazyloadCSS href="/styles/AppleSDGothicNeo.css" />
     <Component {...pageProps} />
