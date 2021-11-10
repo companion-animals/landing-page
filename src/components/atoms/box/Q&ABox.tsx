b54e5d25df72fed03b93c6a40ddf0cf1ef60da70
Seoyul <seoyul.kim@beautydatalab.com>
2021-11-11 03:22:37 +0900

    feat: faq 섹션 모바일 버전 추가

--- src/components/atoms/box/Q&ABox.tsx
+++ src/components/atoms/box/Q&ABox.tsx
@@ -3,42 +3,60 @@ import styled from "styled-components";
 import { Light26 } from "src/components/atoms/text/Typographies";
 
 interface Props {
+  className?: string;
   title: string;
   content: string;
 }
 
-const TitleBox = styled.div`
+const Container = styled.div`
   width: 1000px;
+
+  @media screen and (max-width: 750px) {
+    width: 573px;
+  }
+`;
+
+const TitleBox = styled.div`
   background-color: #7065e7;
-  padding-left: 33px;
-  padding-top: 15px;
-  padding-bottom: 15px;
+  border: 2px solid #b7b7b7;
 `;
 
 const ContentBox = styled.div`
   background-color: #ffffff;
-  padding-left: 33px;
-  padding-top: 20px;
-  padding-bottom: 20px;
+  border: 2px solid #b7b7b7;
+
+  @media screen and (max-width: 750px) {
+    padding-right: 30px;
+  }
 `;
 
 const Content = styled(Light26)`
   white-space: pre-line;
+  margin-left: 33px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+
+  @media screen and (max-width: 750px) {
+    white-space: normal;
+  }
 `;
 
 const Title = styled(Content)`
   color: #ffffff;
+  margin-left: 33px;
+  margin-top: 15px;
+  margin-bottom: 15px;
 `;
 
-const QNABox = ({ title, content }: Props) => (
-  <>
+const QNABox = ({ className, title, content }: Props) => (
+  <Container className={className}>
     <TitleBox>
       <Title>{title}</Title>
     </TitleBox>
     <ContentBox>
       <Content>{content}</Content>
     </ContentBox>
-  </>
+  </Container>
 );
 
 export default QNABox;
