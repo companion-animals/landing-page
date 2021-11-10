015c0d1c37bf6ca6a9b91a39018e265041b55d14
Seoyul <seoyul.kim@beautydatalab.com>
2021-11-11 01:04:49 +0900

    mod: 변경된 UI에 맞춰 수정

--- src/components/molecules/section/landingPage/DetailSection.tsx
+++ src/components/molecules/section/landingPage/DetailSection.tsx
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 import {
   ExtraBold56,
+  ExtraBold48,
   ExtraBold68,
-  ExtraBold38,
+  SemiBold38,
   Light38,
 } from "src/components/atoms/text/Typographies";
 
@@ -24,6 +25,7 @@ const Title = styled(ExtraBold56)`
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   margin-bottom: 58px;
 `;
@@ -32,35 +34,21 @@ const Number = styled(ExtraBold68)`
   color: #7065e7;
 `;
 
-const ExampleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-left: 20px;
-`;
-
-const DetailtWrapper = styled.div`
-  display: inline;
-`;
-
-const Detail = styled(Light38)`
-  color: #6c6c6c;
-  display: inline;
-`;
-
-const BoldDetail = styled(ExtraBold38)`
+const DetailLight = styled(Light38)`
   color: #252525;
   display: inline;
+  white-space: pre-line;
 `;
 
-const Example = styled(Detail)`
-  display: block;
-  color: #8d8d8d;
-`;
-
-const DetailContainer = styled.div`
+const DetailSemiBold = styled(SemiBold38)`
+  color: #000000;
   display: inline;
-  margin-left: 20px;
+  white-space: pre-line;
+  margin-top: 37px;
+`;
+
+const BoldDetail = styled(ExtraBold48)`
+  color: #7065e7;
 `;
 
 const DetailSection = () => (
@@ -68,27 +56,28 @@ const DetailSection = () => (
     <Title>{`검증된 특수동물 전문 펫시터니까\n믿을 수 있어요.`}</Title>
     <Wrapper>
       <Number>1.</Number>
-      <ExampleContainer>
-        <DetailtWrapper>
-          <Detail>해당 반려동물 종류</Detail>
-          <BoldDetail> 키워본 경험 1년 이상</BoldDetail>
-        </DetailtWrapper>
-        <Example>(ex- 게코의 경우 게코 1년이상 사육경험 필수)</Example>
-      </ExampleContainer>
+      <BoldDetail>풍부한 반려경험 보유</BoldDetail>
+      <DetailSemiBold>
+        최소 2년 이상 해당 특수동물 반려경험
+        <DetailLight>{`이 있는\n펫시터만을 매칭해드려요.`}</DetailLight>
+      </DetailSemiBold>
     </Wrapper>
     <Wrapper>
       <Number>2.</Number>
-      <DetailContainer>
-        <Detail>자격 요건 확인 및</Detail>
-        <BoldDetail> 신원 확인 완료</BoldDetail>
-      </DetailContainer>
+      <BoldDetail>자격요건 및 신원확인 완료</BoldDetail>
+      <DetailSemiBold>
+        <DetailLight>{`최대 4단계에 걸쳐 특수동물 돌봄에 적합한\n`}</DetailLight>
+        인성과 환경을 갖춘 펫시터만을 선정<DetailLight>해요.</DetailLight>
+      </DetailSemiBold>
     </Wrapper>
     <Wrapper>
       <Number>3.</Number>
-      <DetailContainer>
-        <Detail>최대 4단계에 걸친</Detail>
-        <BoldDetail> 환경 및 인성 검증</BoldDetail>
-      </DetailContainer>
+      <BoldDetail>안심 서비스 지원</BoldDetail>
+      <DetailSemiBold>
+        <DetailLight>{`예상치 못한 사건 `}</DetailLight>
+        {`사고 발생시 즉각 대응이 가능한\n특수동물 전문 안심 서비스`}
+        <DetailLight>를 지원해드려요.</DetailLight>
+      </DetailSemiBold>
     </Wrapper>
   </Container>
 );
