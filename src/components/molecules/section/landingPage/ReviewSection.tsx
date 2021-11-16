4d622a2bb6d0081ef9509b7abc22a8d190857ff8
Seoyul <seoyul.kim@beautydatalab.com>
2021-11-16 23:09:19 +0900

    feat: 리뷰 섹션 애니메이션 추가

--- src/components/molecules/section/landingPage/ReviewSection.tsx
+++ src/components/molecules/section/landingPage/ReviewSection.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import CardWithQuotationMark from "src/components/atoms/card/cardWithQuotationMark";
 import { ExtraBold44, Light44 } from "src/components/atoms/text/Typographies";
+import useScrollFadeIn from "src/hooks/useScrollFadeIn";
 
 const Container = styled.div`
   width: 100%;
@@ -57,23 +58,38 @@ const Card = styled(CardWithQuotationMark)`
   }
 `;
 
-const ReviewSection = () => (
-  <Container>
-    <TitleWrapper>
-      <LightText>특별한 아이들의</LightText>
-      <BoldText>특별한 후기</BoldText>
-    </TitleWrapper>
-    <ContentWrapper>
-      <Card
-        content={`저희 햄찌가 많이 예민한\n아이라 걱정이 많았는데\n건강하게 보살펴주셔서\n덕분에 맘놓고\n여행 다녀올 수 있었네요\n감사합니다 ♥`}
-      />
-      <Card
-        content={`케이지랑 사육환경이\n너무 잘 갖춰진 시터님이라 \n안심하고 맡겼어요!\n다음에도 맡길 일 있으면\n무조건 특별반 통해\n맡길거 같아요`}
-      />
-      <Card
-        content={`벌써 두번째인데\n밑고 맏기게 되네요.\n항상  먼저 전화주시고\n저희 게코 잘 챙겨주셔서 \n감사합니다.`}
-      />
-    </ContentWrapper>
-  </Container>
-);
+const ReviewSection = () => {
+  const firstCardAnimatedItems = useScrollFadeIn({ direction: "up" });
+  const secondCardAnimatedItems = useScrollFadeIn({
+    direction: "up",
+    delay: 200,
+  });
+  const thirdCardAnimatedItems = useScrollFadeIn({
+    direction: "up",
+    delay: 400,
+  });
+
+  return (
+    <Container>
+      <TitleWrapper>
+        <LightText>특별한 아이들의</LightText>
+        <BoldText>특별한 후기</BoldText>
+      </TitleWrapper>
+      <ContentWrapper>
+        <Card
+          animatedItem={firstCardAnimatedItems}
+          content={`저희 햄찌가 많이 예민한\n아이라 걱정이 많았는데\n건강하게 보살펴주셔서\n덕분에 맘놓고\n여행 다녀올 수 있었네요\n감사합니다 ♥`}
+        />
+        <Card
+          animatedItem={secondCardAnimatedItems}
+          content={`케이지랑 사육환경이\n너무 잘 갖춰진 시터님이라 \n안심하고 맡겼어요!\n다음에도 맡길 일 있으면\n무조건 특별반 통해\n맡길거 같아요`}
+        />
+        <Card
+          animatedItem={thirdCardAnimatedItems}
+          content={`벌써 두번째인데\n밑고 맏기게 되네요.\n항상  먼저 전화주시고\n저희 게코 잘 챙겨주셔서 \n감사합니다.`}
+        />
+      </ContentWrapper>
+    </Container>
+  );
+};
 export default ReviewSection;
