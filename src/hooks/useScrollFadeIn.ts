a49b77aecdc6b73040e8c75d5c56a341b823119b
Seoyul <seoyul.kim@beautydatalab.com>
2021-11-16 22:47:28 +0900

    mod: useScrollFadeIn hook 이름 변경 및 direction param으로 받아 애니메이션 적용하도록 추가

--- src/hooks/useScrollFadeIn.ts
+++ src/hooks/useScrollFadeIn.ts
@@ -1,8 +1,24 @@
 import { useCallback, useEffect, useRef } from "react";
 import "intersection-observer";
 
-const useScrollFadeInUp = (delay?: number) => {
+type Direction = "right" | "left" | "up" | "down";
+
+interface ScrollFadeInParam {
+  delay?: number;
+  direction: Direction;
+}
+
+const useScrollFadeIn = (param: ScrollFadeInParam) => {
   const dom = useRef<HTMLDivElement>(null);
+  const delay = param?.delay ?? 0;
+  const transform =
+    param?.direction === "up"
+      ? "translate3d(0, 20%, 0)"
+      : param?.direction === "down"
+      ? "translate3d(0, -20%, 0)"
+      : param?.direction === "right"
+      ? "translate3d(-10%, 0, 0)"
+      : "translate3d(10%, 0, 0)";
 
   const handleScroll = useCallback(
     ([entry]) => {
@@ -40,9 +56,9 @@ const useScrollFadeInUp = (delay?: number) => {
     ref: dom,
     style: {
       opacity: 0,
-      transform: "translate3d(0, 20%, 0)",
+      transform,
     },
   };
 };
 
-export default useScrollFadeInUp;
+export default useScrollFadeIn;
