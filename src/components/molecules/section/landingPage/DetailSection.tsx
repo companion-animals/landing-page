d6d2749a46b93a62134ae4b426c7e7ee056b2982
Seoyul <zoeyul07@gmail.com>
2021-11-17 20:51:03 +0900

    feat: detail section에 애니메이션 추가

--- src/components/molecules/section/landingPage/DetailSection.tsx
+++ src/components/molecules/section/landingPage/DetailSection.tsx
@@ -8,6 +8,7 @@ import {
   Light32,
   Light20,
 } from "src/components/atoms/text/Typographies";
+import useScrollFadeIn from "src/hooks/useScrollFadeIn";
 
 const Container = styled.div`
   width: 100%;
@@ -90,42 +91,47 @@ const MobileTitle = styled(ExtraBold32)`
   }
 `;
 
-const DetailSection = () => (
-  <Container>
-    <MobileTitle>{`검증된\n특수동물 전문 펫시터니까\n믿을 수 있어요.`}</MobileTitle>
-    <Title>{`검증된 특수동물 전문 펫시터니까\n믿을 수 있어요.`}</Title>
-    <Wrapper>
-      <Number>1.</Number>
-      <BoldDetail>풍부한 반려경험 보유</BoldDetail>
-      <DetailSemiBold>
-        최소 2년 이상 해당 특수동물 반려경험
-        <DetailLight>{`이 있는\n펫시터만을 매칭해드려요.`}</DetailLight>
-      </DetailSemiBold>
-      <MobileText>{`최소 2년 이상 해당 특수동물
+const DetailSection = () => {
+  const firstAnimatedItems = useScrollFadeIn({ direction: "up" });
+  const secondAnimatedItems = useScrollFadeIn({ direction: "up" });
+  const thirdAnimatedItems = useScrollFadeIn({ direction: "up" });
+  return (
+    <Container>
+      <MobileTitle>{`검증된\n특수동물 전문 펫시터니까\n믿을 수 있어요.`}</MobileTitle>
+      <Title>{`검증된 특수동물 전문 펫시터니까\n믿을 수 있어요.`}</Title>
+      <Wrapper {...firstAnimatedItems}>
+        <Number>1.</Number>
+        <BoldDetail>풍부한 반려경험 보유</BoldDetail>
+        <DetailSemiBold>
+          최소 2년 이상 해당 특수동물 반려경험
+          <DetailLight>{`이 있는\n펫시터만을 매칭해드려요.`}</DetailLight>
+        </DetailSemiBold>
+        <MobileText>{`최소 2년 이상 해당 특수동물
        반려경험 필수`}</MobileText>
-    </Wrapper>
-    <Wrapper>
-      <Number>2.</Number>
-      <BoldDetail>자격요건 및 신원확인 완료</BoldDetail>
-      <DetailSemiBold>
-        <DetailLight>{`최대 4단계에 걸쳐 특수동물 돌봄에 적합한\n`}</DetailLight>
-        인성과 환경을 갖춘 펫시터만을 선정<DetailLight>해요.</DetailLight>
-      </DetailSemiBold>
-      <MobileText>{`최대 4단계에 결친
+      </Wrapper>
+      <Wrapper {...secondAnimatedItems}>
+        <Number>2.</Number>
+        <BoldDetail>자격요건 및 신원확인 완료</BoldDetail>
+        <DetailSemiBold>
+          <DetailLight>{`최대 4단계에 걸쳐 특수동물 돌봄에 적합한\n`}</DetailLight>
+          인성과 환경을 갖춘 펫시터만을 선정<DetailLight>해요.</DetailLight>
+        </DetailSemiBold>
+        <MobileText>{`최대 4단계에 결친
       환경 & 인성 검증`}</MobileText>
-    </Wrapper>
-    <Wrapper>
-      <Number>3.</Number>
-      <BoldDetail>안심 서비스 지원</BoldDetail>
-      <DetailSemiBold>
-        <DetailLight>{`예상치 못한 사건 `}</DetailLight>
-        {`사고 발생시 즉각 대응이 가능한\n특수동물 전문 안심 서비스`}
-        <DetailLight>를 지원해드려요.</DetailLight>
-      </DetailSemiBold>
-      <MobileText>{`사고 발생시 즉각 대응 가능한 
+      </Wrapper>
+      <Wrapper {...thirdAnimatedItems}>
+        <Number>3.</Number>
+        <BoldDetail>안심 서비스 지원</BoldDetail>
+        <DetailSemiBold>
+          <DetailLight>{`예상치 못한 사건 `}</DetailLight>
+          {`사고 발생시 즉각 대응이 가능한\n특수동물 전문 안심 서비스`}
+          <DetailLight>를 지원해드려요.</DetailLight>
+        </DetailSemiBold>
+        <MobileText>{`사고 발생시 즉각 대응 가능한 
       특수동물 전문 안심서비스 지원`}</MobileText>
-    </Wrapper>
-  </Container>
-);
+      </Wrapper>
+    </Container>
+  );
+};
 
 export default DetailSection;
