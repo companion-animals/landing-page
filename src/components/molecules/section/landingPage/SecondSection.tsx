6e7d5b36fadbb7f7b041a6f1d8a3eb3136cdc0ab
Seoyul <zoeyul07@gmail.com>
2021-11-19 01:27:06 +0900

    mod: 두번째 섹션 캐릭터 이미지로 변경

--- src/components/molecules/section/landingPage/SecondSection.tsx
+++ src/components/molecules/section/landingPage/SecondSection.tsx
@@ -6,12 +6,11 @@ import images from "src/images";
 
 const Container = styled.div`
   width: 100%;
-  max-width: 1180px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding-top: 130px;
+  padding-top: 100px;
   padding-bottom: 80px;
+  background-color: #fafafa;
+  display: flex;
+  justify-content: center;
 
   @media screen and (max-width: 750px) {
     flex-direction: column;
@@ -21,18 +20,17 @@ const Container = styled.div`
   }
 `;
 
-const AnimalImage = styled.img.attrs({ src: images.animals })`
-  height: 500px;
-
-  @media screen and (max-width: 750px) {
-    height: 400px;
-  }
+const Wrapper = styled.div`
+  max-width: 1180px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `;
 
 const TitleContainer = styled.div`
   white-space: pre-line;
-  margin-right: 142px;
-  margin-top: 70px;
+  margin-left: 100px;
+  margin-top: 97px;
 
   @media screen and (max-width: 750px) {
     margin-right: 0px;
@@ -63,6 +61,81 @@ const ColoredBoldTitle = styled(BoldTitle)`
 
 const TitleWrapper = styled.div``;
 
+const ImageContainer = styled.div`
+  position: relative;
+  width: 360px;
+  height: 500px;
+`;
+
+const ImageWrapper = styled.div`
+  background-color: #ffffff;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`;
+
+const BirdWrapper = styled(ImageWrapper)`
+  width: 140px;
+  height: 140px;
+  left: 80px;
+`;
+
+const ChameleonWrapper = styled(ImageWrapper)`
+  width: 110px;
+  height: 110px;
+  right: 0px;
+  top: 90px;
+`;
+
+const HamsterWrapper = styled(ImageWrapper)`
+  width: 160px;
+  height: 160px;
+  top: 170px;
+  left: 0px;
+`;
+
+const ParrotWrapper = styled(ImageWrapper)`
+  width: 140px;
+  height: 140px;
+  right: 15px;
+  top: 230px;
+`;
+
+const PigeonWrapper = styled(ImageWrapper)`
+  width: 129px;
+  height: 129px;
+  top: 350px;
+  left: 50px;
+`;
+
+const RabbitWrapper = styled(ImageWrapper)`
+  width: 100px;
+  height: 100px;
+  right: 30px;
+  top: 400px;
+`;
+
+const Bird = styled.img.attrs({ src: images.bird })`
+  width: 110px;
+`;
+const Chameleon = styled.img.attrs({ src: images.chameleon })`
+  width: 80px;
+`;
+const Hamster = styled.img.attrs({ src: images.hamster })`
+  width: 120px;
+`;
+const Parrot = styled.img.attrs({ src: images.parrot })`
+  width: 105px;
+`;
+const Pigeon = styled.img.attrs({ src: images.pigeon })`
+  width: 100px;
+`;
+const Rabbit = styled.img.attrs({ src: images.rabbit })`
+  width: 80px;
+`;
+
 const SecondSection = () => {
   const lightTitleAnimatedItem = useScrollFadeIn({ direction: "up" });
   const boldTitleAnimatedItem = useScrollFadeIn({
@@ -72,16 +145,37 @@ const SecondSection = () => {
 
   return (
     <Container>
-      <TitleContainer>
-        <TitleWrapper {...lightTitleAnimatedItem}>
-          <LightTitle>고양이말고.. 강아지말고..</LightTitle>
-        </TitleWrapper>
-        <TitleWrapper {...boldTitleAnimatedItem}>
-          <ColoredBoldTitle>{`햄스터, 기니피그, 토끼\n도마뱀, 앵무새...`}</ColoredBoldTitle>
-          <BoldTitle>돌봐줄 곳 어디 없을까요?</BoldTitle>
-        </TitleWrapper>
-      </TitleContainer>
-      <AnimalImage />
+      <Wrapper>
+        <ImageContainer>
+          <BirdWrapper>
+            <Bird />
+          </BirdWrapper>
+          <ChameleonWrapper>
+            <Chameleon />
+          </ChameleonWrapper>
+          <HamsterWrapper>
+            <Hamster />
+          </HamsterWrapper>
+          <ParrotWrapper>
+            <Parrot />
+          </ParrotWrapper>
+          <PigeonWrapper>
+            <Pigeon />
+          </PigeonWrapper>
+          <RabbitWrapper>
+            <Rabbit />
+          </RabbitWrapper>
+        </ImageContainer>
+        <TitleContainer>
+          <TitleWrapper {...lightTitleAnimatedItem}>
+            <LightTitle>고양이말고, 강아지말고</LightTitle>
+          </TitleWrapper>
+          <TitleWrapper {...boldTitleAnimatedItem}>
+            <ColoredBoldTitle>{}</ColoredBoldTitle>
+            <BoldTitle>{`햄스터, 기니피그, 토끼\n도마뱀, 앵무새\n돌봐줄 곳 어디 없을까요?`}</BoldTitle>
+          </TitleWrapper>
+        </TitleContainer>
+      </Wrapper>
     </Container>
   );
 };
