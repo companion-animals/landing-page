d29a86d176aa689efabbc5b9f92223c3bb767f13
Seoyul <zoeyul07@gmail.com>
2021-11-19 01:26:44 +0900

    feat: 동물 캐릭터 이미지 추가

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,19 +1,16 @@
 const images = {
-  considerEmoji: "/images/consider_emoji.png",
-  sadEmoji: "/images/sad_emoji.png",
-  tearsEmoji: "/images/tears_emoji.png",
-  emailIcon: "/images/email_icon.png",
-  embarrassmentEmoji: "/images/embarrassment_emoji.png",
-  rabbit: "/images/rabbit.png",
-  parrot: "/images/parrot.png",
-  rabbitBG: "/images/rabbit_bg.png",
-  rightSpeechBubble: "/images/speech_bubble_right.png",
-  leftSpeechBubble: "/images/speech_bubble_left.png",
   animals: "/images/animals.png",
   logo: "/images/specialban_logo.png",
   character: "/images/specialban_character_logo.png",
   wideLogo: "/images/specialban_wide_logo.png",
   logoOG: "/images/specialban_og_image.png",
+  bird: "images/bird.png",
+  chameleon: "/images/chameleon.png",
+  hamster: "/images/hamster.png",
+  parrot: "/images/parrot.png",
+  pigeon: "/images/pigeon.png",
+  rabbit: "images/rabbit.png",
+  snake: "/images/snake.png",
 };
 
 export default images;
