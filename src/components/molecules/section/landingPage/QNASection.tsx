27bffccffcf0bde42b6cd732ef65dc3da3505d0a
specialban <specialban.official@gmail.com>
2021-11-23 23:45:45 +0900

    feat: add company info

--- src/components/molecules/section/landingPage/QNASection.tsx
+++ src/components/molecules/section/landingPage/QNASection.tsx
@@ -28,7 +28,11 @@ const Text = styled(Medium36)`
   }
 `;
 
-const Footer = styled(Light16)`
+const Footer = styled.footer`
+  padding: 0 16px;
+`;
+
+const Info = styled(Light16)`
   color: #868686;
 
   @media screen and (max-width: 900px) {
@@ -94,7 +98,14 @@ const QNASection = () => (
       </Circle>
       <Email>specialban.official@gmail.com</Email>
     </BorderChip>
-    <Footer>{`ⓒ 2021 우리동네특별반\nAll Rights Reserved`}</Footer>
+    <Footer>
+      <Info>
+        드로우코퍼레이션| 대표: 박진주 | 사업자등록번호: 594-45-00660 |
+        통신판매업신고번호: 제 2021-서울중랑-1384호
+      </Info>
+      <Info>서울시 중랑구 봉화산로 193 | 1103-1207</Info>
+      <Info>{`ⓒ 2021 우리동네특별반\nAll Rights Reserved`}</Info>
+    </Footer>
   </Container>
 );
 
