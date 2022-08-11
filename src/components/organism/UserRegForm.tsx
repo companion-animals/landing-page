bf071ca8e33ddd105ee7f26ae6791fa6b183143f
LSJJOON <whitour@gmail.com>
2022-08-12 04:17:20 +0900

    feat: add hook form logic

--- src/components/organism/UserRegForm.tsx
+++ src/components/organism/UserRegForm.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import tw from "twin.macro";
 
@@ -39,41 +40,50 @@ const Check = styled(CheckBoxWithLabel)`
   ${tw`mt-2`}
 `;
 
-const UserRegForm = () => (
-  <Form>
-    <Input name="name" label="이름" type="text" />
-    <Input name="email" label="이메일(ID)" type="email" />
-    <Input name="birth" label="생년월일" type="date" />
-    <SexContainer>
-      <Radio label="남자" name="sex" value="man" />
-      <Radio label="여자" name="sex" value="woman" />
-    </SexContainer>
+const UserRegForm = () => {
+  const { register, handleSubmit } = useForm<UserRegFormData>();
+  const onSubmit = (data: UserRegFormData) => console.log(data);
 
-    <Input name="password" label="비밀번호" type="password" />
-    <Input name="password-check" label="비밀번호 확인" type="password" />
-    <Check
-      label={
-        <Link href="https://www.notion.so/57df7f12dae34ac0b193850620a9d409">
-          우리동네 특별반 이용약관 동의 (필수)
-        </Link>
-      }
-    />
-    <Check
-      label={
-        <Link href="https://www.notion.so/837912043a0844cfb15032a0ee345700">
-          개인정보 처리방침 동의 (필수)
-        </Link>
-      }
-    />
-    <Check
-      label={
-        <Link href="https://www.notion.so/e302edc5d39744b2a6c4215ed335cf3e">
-          펫시터 약관 동의 (필수)
-        </Link>
-      }
-    />
-    <Submit type="submit">회원가입</Submit>
-  </Form>
-);
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Input {...register("name")} label="이름" type="text" />
+      <Input {...register("email")} label="이메일(ID)" type="email" />
+      <Input {...register("birth")} label="생년월일" type="date" />
+      <SexContainer>
+        <Radio {...register("sex")} label="남자" value="man" />
+        <Radio {...register("sex")} label="여자" value="woman" />
+      </SexContainer>
+
+      <Input {...register("password")} label="비밀번호" type="password" />
+      <Input
+        {...register("password-check")}
+        label="비밀번호 확인"
+        type="password"
+      />
+      <Check
+        label={
+          <Link href="https://www.notion.so/57df7f12dae34ac0b193850620a9d409">
+            우리동네 특별반 이용약관 동의 (필수)
+          </Link>
+        }
+      />
+      <Check
+        label={
+          <Link href="https://www.notion.so/837912043a0844cfb15032a0ee345700">
+            개인정보 처리방침 동의 (필수)
+          </Link>
+        }
+      />
+      <Check
+        label={
+          <Link href="https://www.notion.so/e302edc5d39744b2a6c4215ed335cf3e">
+            펫시터 약관 동의 (필수)
+          </Link>
+        }
+      />
+      <Submit type="submit">회원가입</Submit>
+    </Form>
+  );
+};
 
 export default UserRegForm;
