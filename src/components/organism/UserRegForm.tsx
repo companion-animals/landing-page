1564189f07fd4a09f87cd71a4e001f38694295cb
LSJJOON <whitour@gmail.com>
2022-08-18 15:11:58 +0900

    feat: add UserRegForm error case logic

--- src/components/organism/UserRegForm.tsx
+++ src/components/organism/UserRegForm.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import tw from "twin.macro";
+import * as yup from "yup";
 
 import BasicButton from "src/components/atom/button/BasicButton";
 import BasicInputField from "src/components/molecules/BasicInputField";
@@ -44,9 +46,43 @@ const Check = styled(CheckBoxWithLabel)`
   ${tw`mt-2`}
 `;
 
+const userRegSchema = yup
+  .object({
+    name: yup.string().required("이름을 입력해주세요"),
+    email: yup
+      .string()
+      .email("이메일 형식이 아닙니다")
+      .required("이메일을 입력해주세요"),
+    birth: yup
+      .date()
+      .min("1900.12.31", "생년월일을 다시 확인해주세요")
+      .required("생년월일을 입력해주세요"),
+    password: yup
+      .string()
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+        "최소 8글자, 영문과 숫자의 조합으로 이루어져야 합니다",
+      )
+      .required("비밀번호를 입력해주세요"),
+    "password-check": yup
+      .string()
+      .test(
+        "password check",
+        "비밀번호가 일치하지 않습니다",
+        (value, context) => context.parent.password === value,
+      )
+      .required("비밀번호 확인을 입력해주세요"),
+    sex: yup.string().oneOf(["man", "woman"]).required("성별을 선택해주세요"),
+  })
+  .required();
+
 const UserRegForm = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const { register, handleSubmit } = useForm<UserRegFormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserRegFormData>({ resolver: yupResolver(userRegSchema) });
   const onSubmit = async (data: UserRegFormData) => {
     if (isLoading) {
       return;
@@ -70,19 +106,40 @@ const UserRegForm = () => {
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <Input {...register("name")} label="이름" type="text" />
-      <Input {...register("email")} label="이메일(ID)" type="email" />
-      <Input {...register("birth")} label="생년월일" type="date" />
+      <Input
+        {...register("name")}
+        label="이름"
+        type="text"
+        errorMessage={errors.name?.message}
+      />
+      <Input
+        {...register("email")}
+        label="이메일(ID)"
+        type="email"
+        errorMessage={errors.email?.message}
+      />
+      <Input
+        {...register("birth", { valueAsDate: true })}
+        label="생년월일"
+        type="date"
+        errorMessage={errors.birth?.message}
+      />
       <SexContainer>
         <Radio {...register("sex")} label="남자" value="man" />
         <Radio {...register("sex")} label="여자" value="woman" />
       </SexContainer>
 
-      <Input {...register("password")} label="비밀번호" type="password" />
+      <Input
+        {...register("password")}
+        label="비밀번호"
+        type="password"
+        errorMessage={errors.password?.message}
+      />
       <Input
         {...register("password-check")}
         label="비밀번호 확인"
         type="password"
+        errorMessage={errors["password-check"]?.message}
       />
       <Check
         label={
