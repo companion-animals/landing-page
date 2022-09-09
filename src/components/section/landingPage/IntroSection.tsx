86d720019805896d8f3190cee6d87f8ef0b91cd9
LSJJOON <whitour@gmail.com>
2022-09-09 14:44:38 +0900

    feat: add mobile login button

--- src/components/section/landingPage/IntroSection.tsx
+++ src/components/section/landingPage/IntroSection.tsx
@@ -37,9 +37,20 @@ const IntroSection = () => {
               </NextLink>
             )}
           </TitleWrapper>
-          <Link id="floatingFindHotelButton" href={APPLY_LINK} target="_blank">
-            <MobileSignUpButton width={width} title="지금 신청하기" />
-          </Link>
+          {isLogin ? (
+            <Link
+              id="floatingFindHotelButton"
+              href={APPLY_LINK}
+              target="_blank"
+            >
+              <MobileSignUpButton width={width} title="지금 신청하기" />
+            </Link>
+          ) : (
+            <NextLink href="/login">
+              <MobileSignUpButton width={width} title="지금 신청하기" />
+            </NextLink>
+          )}
+
           <Illustration />
         </ContentWrapper>
       </Wrapper>
