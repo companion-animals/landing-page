71576ada7919d3c1d8f63edb95973f85f66ec681
LSJJOON <whitour@gmail.com>
2022-09-13 03:13:28 +0900

    feat: add order schema

---
+++ src/db/models/Order.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+export interface OrderModel {
+  price: number;
+  sitterName: string;
+  sittingStartDate: Date;
+  sittingEndDate: Date;
+  animalCategory: string;
+  animalName: string;
+  userID: mongoose.Types.ObjectId;
+  sitterPhoneNum: string;
+  isPaid: boolean;
+}
+
+const orderSchema = new mongoose.Schema({
+  price: Number,
+  sitterName: String,
+  sittingStartDate: Date,
+  sittingEndDate: Date,
+  animalCategory: String,
+  animalName: String,
+  userID: mongoose.Schema.Types.ObjectId,
+  sitterPhoneNum: String,
+  isPaid: Boolean,
+});
+
+const Order =
+  (mongoose.models.Order as mongoose.Model<OrderModel>) ||
+  mongoose.model<OrderModel>("Order", orderSchema);
+
+export default Order;
