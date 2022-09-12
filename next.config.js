a60647ade09945479fbd97b1d36a842239781ab3
LSJJOON <whitour@gmail.com>
2022-09-13 03:29:16 +0900

    feat: upgrade nextjs and set superjson

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({ reactStrictMode: true });
+module.exports = withBundleAnalyzer({
+	reactStrictMode: true, 
+	experimental: {
+		swcPlugins: [
+			[
+				'next-superjson-plugin',
+				{
+					excluded: [],
+				},
+			],
+		],
+	}
+});
