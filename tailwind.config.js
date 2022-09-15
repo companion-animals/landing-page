d9db8b87b9e3cbd8f3b68411bffe0c15042bffda
LSJJOON <whitour@gmail.com>
2022-09-15 14:16:13 +0900

    feat: add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         primary: "#79cea7",
+      },
+      keyframes: {
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-up": "fade-in-up 0.5s ease-out",
       },
     },
   },
