6e51c864b3cbf96dce95461660744846b2692e23
LSJJOON <whitour@gmail.com>
2022-09-16 11:46:13 +0900

    feat: add terms links at footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 import { Light16 } from "src/components/text/Typographies";
+import {
+  TERMS_OF_SERVICE,
+  SITTER_TERMS,
+  PRIVACY_POLICY,
+} from "src/constatnts/links";
 
 const Footer = () => (
   <FooterContainer>
@@ -9,7 +14,13 @@ const Footer = () => (
       통신판매업신고번호: 제 2021-서울중랑-1384호
     </Info>
     <Info>서울시 중랑구 봉화산로 193 | 대표전화: 010-5716-6127</Info>
-    <Info>{`ⓒ 2021 우리동네특별반\nAll Rights Reserved`}</Info>
+    <Info>ⓒ 2021 우리동네특별반 All Rights Reserved</Info>
+    <Info>
+      <Link href={TERMS_OF_SERVICE}>이용약관</Link> |{" "}
+      <Link href={PRIVACY_POLICY}>개인정보 처리방침</Link> |{" "}
+      <Link href={SITTER_TERMS}>펫시터 약관</Link>
+    </Info>
+    <Info />
   </FooterContainer>
 );
 
@@ -28,4 +39,10 @@ const Info = styled(Light16)`
     line-height: 22px;
     font-size: 12px;
   }
+`;
+
+const Link = styled.a.attrs({ target: "_blank", rel: "noreferrer" })`
+  text-decoration: none;
+  display: contents;
+  color: #868686;
 `;
